b7f134209d96a58b8a208a8ec7f8e8b836d7fb97
mrbrunelli <matheus.brunelli@gmail.com>
2023-04-10 18:13:58 -0300

    add put endpoint to update a topic

--- src/main/kotlin/br/com/alura/forum/controller/TopicController.kt
+++ src/main/kotlin/br/com/alura/forum/controller/TopicController.kt
@@ -2,10 +2,12 @@ package br.com.alura.forum.controller
 
 import br.com.alura.forum.dto.NewTopicForm
 import br.com.alura.forum.dto.TopicView
+import br.com.alura.forum.dto.UpdateTopicForm
 import br.com.alura.forum.service.TopicService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -21,5 +23,8 @@ class TopicController(private val service: TopicService) {
     fun findById(@PathVariable id: Long): TopicView? = service.findById(id)
 
     @PostMapping
-    fun create(@RequestBody @Valid dto: NewTopicForm) = service.create(dto)
+    fun create(@RequestBody @Valid form: NewTopicForm) = service.create(form)
+
+    @PutMapping
+    fun update(@RequestBody @Valid form: UpdateTopicForm) = service.update(form)
 }
--- src/main/kotlin/br/com/alura/forum/service/TopicService.kt
+++ src/main/kotlin/br/com/alura/forum/service/TopicService.kt
@@ -2,6 +2,7 @@ package br.com.alura.forum.service
 
 import br.com.alura.forum.dto.NewTopicForm
 import br.com.alura.forum.dto.TopicView
+import br.com.alura.forum.dto.UpdateTopicForm
 import br.com.alura.forum.mapper.NewTopicFormMapper
 import br.com.alura.forum.mapper.TopicViewMapper
 import br.com.alura.forum.model.Topic
@@ -27,5 +28,19 @@ class TopicService(
         val topic = newTopicFormMapper.map(dto)
         topic.id = topics.size.toLong() + 1
         topics.add(topic)
+    }
+
+    fun update(form: UpdateTopicForm) {
+        val topic = topics.find { it.id == form.id} ?: throw Error("Topic not found")
+        topics = topics.minus(topic).plus(Topic(
+            id = form.id,
+            title = form.title,
+            message = form.message,
+            author = topic.author,
+            course = topic.course,
+            answers = topic.answers,
+            status = topic.status,
+            createdAt = topic.createdAt
+        )) as MutableList<Topic>
     }
 }
