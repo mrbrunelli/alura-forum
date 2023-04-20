b6b44b49e3894a607ce575292c6e77761a2d16cb
mrbrunelli <matheus.brunelli@gmail.com>
2023-04-20 16:01:09 -0300

    implement spring security

---
+++ src/main/kotlin/br/com/alura/forum/service/UserDetail.kt
@@ -0,0 +1,20 @@
+package br.com.alura.forum.service
+
+import br.com.alura.forum.model.Author
+import org.springframework.security.core.userdetails.UserDetails
+
+class UserDetail(private val user: Author) : UserDetails {
+    override fun getAuthorities() = null
+
+    override fun getPassword() = user.password
+
+    override fun getUsername() = user.email
+
+    override fun isAccountNonExpired() = true
+
+    override fun isAccountNonLocked() = true
+
+    override fun isCredentialsNonExpired() = true
+
+    override fun isEnabled() = true
+}
---
+++ src/main/resources/db/migration/V6__insert_password_author.sql
@@ -0,0 +1,2 @@
+UPDATE author SET password = '$2a$12$yNv6TzkKkhRETPUJCEvXc.c7hy.Q.evk30bbF0OOwQ85wSeNR5Jdm' WHERE id = 1;
+UPDATE author SET password = '$2a$12$SomIaQA25WdGj9p3ItpNQeYZF8INw7k92sZ4c9EU8XyYjy6ybZ2Mu' WHERE id = 2;
