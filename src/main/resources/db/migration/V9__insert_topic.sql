9378578b746b99b969434cd49685bfc807b9bcc9
mrbrunelli <matheus.brunelli@gmail.com>
2023-04-24 13:39:59 -0300

    add topic seed and add updated at field

---
+++ src/main/resources/db/migration/V9__insert_topic.sql
@@ -0,0 +1,2 @@
+INSERT INTO topic(id, title, message, created_at, status, course_id, author_id)
+    VALUES(1, 'Dúvida de Kotlin', 'Minha função let não funciona', '2023-04-24 12:14:00', 'NOT_ANSWERED', 1, 1);
