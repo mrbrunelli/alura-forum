c205746e8bf67600be1f38fb6166223d6a36fcfe
mrbrunelli <matheus.brunelli@gmail.com>
2023-05-04 14:18:00 -0300

    implement email notification

--- src/main/kotlin/br/com/alura/forum/service/AnswerService.kt
+++ src/main/kotlin/br/com/alura/forum/service/AnswerService.kt
@@ -4,7 +4,6 @@ import br.com.alura.forum.dto.AnswerView
 import br.com.alura.forum.dto.NewAnswerForm
 import br.com.alura.forum.mapper.AnswerViewMapper
 import br.com.alura.forum.mapper.NewAnswerFormMapper
-import br.com.alura.forum.model.Answer
 import br.com.alura.forum.repository.AnswerRepository
 import org.springframework.cache.annotation.CacheEvict
 import org.springframework.cache.annotation.Cacheable
@@ -16,12 +15,15 @@ import org.springframework.stereotype.Service
 class AnswerService(
     private val repository: AnswerRepository,
     private val newAnswerFormMapper: NewAnswerFormMapper,
-    private val answerViewMapper: AnswerViewMapper
+    private val answerViewMapper: AnswerViewMapper,
+    private val emailService: EmailService
 ) {
     @CacheEvict(value = ["answers"], allEntries = true)
     fun create(dto: NewAnswerForm) {
         val answer = newAnswerFormMapper.map(dto)
+        val authorEmail = answer.topic?.author?.email ?: throw RuntimeException("author's email is required")
         repository.save(answer)
+        emailService.notify(authorEmail)
     }
 
     @Cacheable(cacheNames = ["answers"], key = "#root.method.name")
