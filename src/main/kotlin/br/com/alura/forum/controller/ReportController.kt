eb544c706cea3a9721b5f9f24f395088e8f4d2dd
mrbrunelli <matheus.brunelli@gmail.com>
2023-05-04 16:16:03 -0300

    configure thymeleaf model

--- src/main/kotlin/br/com/alura/forum/controller/ReportController.kt
+++ src/main/kotlin/br/com/alura/forum/controller/ReportController.kt
@@ -1,16 +1,17 @@
 package br.com.alura.forum.controller
 
-import br.com.alura.forum.dto.TopicByCategoryDto
 import br.com.alura.forum.service.TopicService
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
 
-@RestController
+@Controller
 @RequestMapping("reports")
 class ReportController(private val topicService: TopicService) {
     @GetMapping
-    fun topicReport(): List<TopicByCategoryDto> {
-        return topicService.report()
+    fun topicReport(model: Model): String {
+        model.addAttribute("topicsByCategory", topicService.report())
+        return "report"
     }
 }
